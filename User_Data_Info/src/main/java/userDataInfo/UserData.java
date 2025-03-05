0fdca5e2d91a16b42c8e5d7b67af351e0fcdfd71
Souravdash222 <souravdash222@gmail.com>
2025-03-05 13:18:26 +0530

    Commit

---
+++ User_Data_Info/src/main/java/userDataInfo/UserData.java
@@ -0,0 +1,36 @@
+package userDataInfo;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class UserData extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+    public UserData() {
+       
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
